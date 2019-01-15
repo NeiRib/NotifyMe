83b81e532b0fd8150ac5991a6bcad741b9360b6d
NeiRib <dp44mj@gmail.com>
2019-01-15 10:56:50 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,37 +5,84 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+
+        <!-- Estilos -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="estilos/main.css" type="text/css"> 
+        <!-- Fuentes -->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+        <!-- Scripts -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="estilos/main.css" type="text/css"> 
+
     </head>
     <body>
 
-        <div align="right"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                <?php echo "<img src='imagenes/".$_SESSION["idioma"].".png'" ?>" alt="Idioma"></a>
+
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top navbar-shrink" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">NotifyMe</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <!--botón menu móvil-->
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#services"><?php echo $menu1; ?></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#portfolio"><?php echo $menu2; ?></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#about"><?php echo $menu3; ?></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#team"><?php echo $menu4; ?></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
 
-            <ul class="dropdown-menu" role="menu" style="min-width:0">
-                <li><a href="?idioma=es"><img src="imagenes/es.png"></a></li>
-                <li><a href="?idioma=cat"><img src="imagenes/cat.png"></a></li>
-                <li><a href="?idioma=en"><img src="imagenes/en.png"></a></li>
-            </ul>
-        </div>
+            
+            <li class="nav-item dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            <?php echo "<img src='imagenes/" . $_SESSION["idioma"] . ".png'" ?>" alt="Idioma"></a>
 
 
-        <br>
-        <?php
-        echo "<a href='#'>" . $menu1 . "</a>" . " ";
-        echo "<a href='#'>" . $menu2 . "</a>" . " ";
-        echo "<a href='#'>" . $menu3 . "</a>" . " ";
-        echo "<a href='#'>" . $menu4 . "</a>" . " ";
+                <ul class="dropdown-menu" role="menu" style="min-width:0">
+                    <li><a href="?idioma=es"><img src="imagenes/es.png"></a></li>
+                    <li><a href="?idioma=cat"><img src="imagenes/cat.png"></a></li>
+                    <li><a href="?idioma=en"><img src="imagenes/en.png"></a></li>
+                </ul>
+            
+             </li>
+            
 
-        ?>
+            
+            <!--
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+                -->
 
+        </nav>
 
-         
 
 
     </body>
